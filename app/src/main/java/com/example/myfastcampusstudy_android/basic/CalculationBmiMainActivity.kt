4d89267f4b673ea5885bec608663997d81e44be4
JuhyeokLee97 <zero5.two4@gmail.com>
2021-10-31 13:31:19 +0900

    BMI 계산기 - 03. 키와 몸무게 입력 화면 기능 구현하기(1)

--- app/src/main/java/com/example/myfastcampusstudy_android/basic/CalculationBmiMainActivity.kt
+++ app/src/main/java/com/example/myfastcampusstudy_android/basic/CalculationBmiMainActivity.kt
@@ -2,11 +2,17 @@ package com.example.myfastcampusstudy_android.basic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.example.myfastcampusstudy_android.R
 
 class CalculationBmiMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculation_bmi_main)
+
+        val etHeight: EditText = findViewById(R.id.etHeight)
+        val etWeight: EditText = findViewById(R.id.etWeight)
+        val btnCalculation: Button = findViewById(R.id.btnCalculation)
     }
 }
