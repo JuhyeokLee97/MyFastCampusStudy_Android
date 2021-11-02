e1fb78bb4ecf4f37014ffb9339e2b382130b1aab
JuhyeokLee97 <zero5.two4@gmail.com>
2021-11-02 23:16:25 +0900

    로또번호 추첨기 - 04 ConstraintLayout을 이용하여 기본 UI 그리기

---
+++ app/src/main/java/com/example/myfastcampusstudy_android/basic/lottery_number_recommendation/LotteryNumberRecommendationMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myfastcampusstudy_android.basic.lottery_number_recommendation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myfastcampusstudy_android.R
+
+class LotteryNumberRecommendationMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lottery_number_recommendation_main)
+    }
+}
