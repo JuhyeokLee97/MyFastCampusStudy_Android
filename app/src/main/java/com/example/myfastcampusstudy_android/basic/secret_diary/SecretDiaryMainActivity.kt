6d7f1239507bd8a33ab4959508a77741d69242b0
JuhyeokLee97 <zero5.two4@gmail.com>
2021-11-09 00:03:54 +0900

    비밀 다이어리 - 02. 자물쇠 페이지 구현하기(2)

--- app/src/main/java/com/example/myfastcampusstudy_android/basic/secret_diary/SecretDiaryMainActivity.kt
+++ app/src/main/java/com/example/myfastcampusstudy_android/basic/secret_diary/SecretDiaryMainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.NumberPicker
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.edit
 import com.example.myfastcampusstudy_android.R
 
 class SecretDiaryMainActivity : AppCompatActivity() {
@@ -51,7 +52,7 @@ class SecretDiaryMainActivity : AppCompatActivity() {
 
     private fun setListener(){
         btnOpenDiary.setOnClickListener {
-            if (!changePasswordMode){
+            if (changePasswordMode){
                 Toast.makeText(this, "비밀번호 변경 중... 지금은 로그인 할 수 없습니다.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
@@ -60,7 +61,7 @@ class SecretDiaryMainActivity : AppCompatActivity() {
 
             if (passwordPreferences.getString("password", "000").equals(passwordFromUser)){
                 // Success
-
+                Toast.makeText(this, "로그인 성공 하였습니다.", Toast.LENGTH_SHORT).show()
             }else{
                 AlertDialog.Builder(this)
                     .setTitle("실패")
@@ -75,7 +76,12 @@ class SecretDiaryMainActivity : AppCompatActivity() {
             val passwordPreferences = getSharedPreferences("password", Context.MODE_PRIVATE)
 
             if (changePasswordMode){    // 비밀번호 변경 중 -> 완료
-
+                Toast.makeText(this, "비밀번호 ${getPasswordFromUser()}로 변경 됨", Toast.LENGTH_SHORT).show()
+                it.setBackgroundColor(Color.BLACK)
+                changePasswordMode = false
+                passwordPreferences.edit(true){
+                    putString("password", getPasswordFromUser())
+                }
             }else{  // -> 비밀번호 변경 가능 모드
                 if(passwordPreferences.getString("password", "000").equals(getPasswordFromUser())){ // 입력된 비밀번호가 같은 경우 => 비밀번호 수정 가능
                     Toast.makeText(this, "비밀번호를 변경 모드 ON", Toast.LENGTH_SHORT).show()
