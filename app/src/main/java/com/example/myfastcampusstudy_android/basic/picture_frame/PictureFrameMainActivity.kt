8b0af7477cc510011bfda77bb9569ec87b58cd33
JuhyeokLee97 <zero5.two4@gmail.com>
2021-11-22 21:46:24 +0900

    전자액자 - 01. 프로젝트 셋팅

---
+++ app/src/main/java/com/example/myfastcampusstudy_android/basic/picture_frame/PictureFrameMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myfastcampusstudy_android.basic.picture_frame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myfastcampusstudy_android.R
+
+class PictureFrameMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_picture_frame_main)
+    }
+}
