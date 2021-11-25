b0cc13e55e65fd359a2f0746ee69ddb98b2c0b3e
JuhyeokLee97 <zero5.two4@gmail.com>
2021-11-26 00:46:10 +0900

    뽀모도로 타이머 - 00. 프로젝트 셋팅

---
+++ app/src/main/java/com/example/myfastcampusstudy_android/basic/pomodoro_timer/PomodoroTimerMainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myfastcampusstudy_android.basic.pomodoro_timer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PomodoroTimerMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pomodoro_timer_main)
+    }
+}
