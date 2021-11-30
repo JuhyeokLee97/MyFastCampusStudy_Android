e7623909fd80cf7f903eeffc01e69d70b10f02a2
JuhyeokLee97 <zero5.two4@gmail.com>
2021-11-30 20:31:50 +0900

    뽀모도로 타이머 - 03. 효과음 추가하기

--- app/src/main/java/com/example/myfastcampusstudy_android/basic/pomodoro_timer/PomodoroTimerMainActivity.kt
+++ app/src/main/java/com/example/myfastcampusstudy_android/basic/pomodoro_timer/PomodoroTimerMainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myfastcampusstudy_android.basic.pomodoro_timer
 
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -18,11 +19,33 @@ class PomodoroTimerMainActivity : AppCompatActivity() {
     private lateinit var seekBar: SeekBar
     private var currentCountDownTimer: CountDownTimer? = null
 
+    private val soundPool = SoundPool.Builder().build()
+    private var tickingSoundId: Int? = null
+    private var bellSoundId: Int? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pomodoro_timer_main)
 
         initView()
+        initSound()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        soundPool.autoResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        soundPool.autoPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        soundPool.release()
     }
 
     private fun initView() {
@@ -33,7 +56,7 @@ class PomodoroTimerMainActivity : AppCompatActivity() {
         seekBar = findViewById(R.id.seekBar)
         seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                if(fromUser)
+                if (fromUser)
                     updateRemainTimes(progress * 60 * 1000L)
             }
 
@@ -44,12 +67,21 @@ class PomodoroTimerMainActivity : AppCompatActivity() {
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
                 seekBar ?: return
-                currentCountDownTimer = createCountDownTimer(seekBar!!.progress * 60 * 1000L)
-                currentCountDownTimer!!.start()
+
+                startCountDown()
             }
         })
     }
 
+
+    private fun startCountDown() {
+        currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L)
+        currentCountDownTimer!!.start()
+
+        tickingSoundId?.let {
+            soundPool.play(it, 1F, 1F, 0, -1, 1F)
+        }
+    }
 
     private fun createCountDownTimer(initialMillis: Long) =
         object : CountDownTimer(initialMillis, 1000L) {
@@ -59,10 +91,19 @@ class PomodoroTimerMainActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {   // 0으로 초기화
-                updateRemainTimes(0)
-                updateSeekBar(0)
+                completeCountDown()
             }
         }
+
+    private fun completeCountDown() {
+        updateRemainTimes(0)
+        updateSeekBar(0)
+
+        soundPool.autoPause()
+        bellSoundId?.let {
+            soundPool.play(it, 1F, 1F, 0, 0, 1F)
+        }
+    }
 
     private fun updateRemainTimes(remainMillis: Long) {
         val remainSeconds = remainMillis / 1000
@@ -77,5 +118,10 @@ class PomodoroTimerMainActivity : AppCompatActivity() {
         val remainMinutes = remainSeconds / 60
 
         seekBar.progress = remainMinutes.toInt()
+    }
+
+    private fun initSound() {
+        tickingSoundId = soundPool.load(this, R.raw.timer_ticking, 1)
+        bellSoundId = soundPool.load(this, R.raw.timer_bell, 1)
     }
 }
