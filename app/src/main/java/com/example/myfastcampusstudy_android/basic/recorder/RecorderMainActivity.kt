4548d957d1b1ccd2b8e86eec64e1ef8c36ccfc65
JuhyeokLee97 <zero5.two4@gmail.com>
2021-12-02 06:10:14 +0900

    녹음기 - 03. 녹음 기능 구현하기(1)

--- app/src/main/java/com/example/myfastcampusstudy_android/basic/recorder/RecorderMainActivity.kt
+++ app/src/main/java/com/example/myfastcampusstudy_android/basic/recorder/RecorderMainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myfastcampusstudy_android.basic.recorder
 
 import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -12,17 +14,42 @@ import java.util.jar.Manifest
 class RecorderMainActivity : AppCompatActivity() {
     private lateinit var btnRecord: RecordButton
     private var state = State.BEFORE_RECORDING
-    private val requiredPermissions = arrayOf(android.Manifest.permission.RECORD_AUDIO)
+        set(value) {
+            field = value
+            btnRecord.updateIconWithState(value)
+        }
 
+    private val requiredPermissions = arrayOf(android.Manifest.permission.RECORD_AUDIO)
+    private val recordingFilePath: String by lazy {
+        "${externalCacheDir?.absolutePath + "/recording.3gp"}"
+    }
+
+    private var recorder: MediaRecorder? = null
+    private var player: MediaPlayer? = null
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recorder_main)
         requestAudioPermission()
         initViews()
+        bindViews()
     }
 
     private fun initViews() {
         initRecordButton()
+    }
+
+
+    private fun bindViews() {
+        btnRecord.setOnClickListener {
+            when (state) {
+                State.BEFORE_RECORDING -> startRecording()
+                State.ON_RECORDING -> stopRecording()
+                State.AFTER_RECORDING -> startPlay()
+                State.ON_PLAYING -> stopPlay()
+            }
+        }
     }
 
     private fun initRecordButton() {
@@ -51,6 +78,44 @@ class RecorderMainActivity : AppCompatActivity() {
             Toast.makeText(this, "마이크 권한을 허용하였습니다.", Toast.LENGTH_SHORT).show()
         }
 
+    }
+
+    private fun startRecording() {
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            setOutputFile(recordingFilePath)
+            prepare()
+        }
+
+        recorder?.start()
+        state = State.ON_RECORDING
+    }
+
+    private fun stopRecording() {
+        recorder?.run {
+            stop()
+            release()
+        }
+        recorder = null
+        state = State.AFTER_RECORDING
+    }
+
+    private fun startPlay() {
+        player = MediaPlayer().apply {
+            setDataSource(recordingFilePath)
+            prepare()
+        }
+
+        player?.start()
+        state = State.ON_PLAYING
+    }
+
+    private fun stopPlay() {
+        player?.release()
+        player = null
+        state = State.AFTER_RECORDING
     }
 
     companion object {
--- app/src/main/java/com/example/myfastcampusstudy_android/basic/recorder/State.kt
+++ app/src/main/java/com/example/myfastcampusstudy_android/basic/recorder/State.kt
@@ -3,6 +3,6 @@ package com.example.myfastcampusstudy_android.basic.recorder
 enum class State {
     BEFORE_RECORDING,
     ON_RECORDING,
-    AFTER_RECODING,
+    AFTER_RECORDING,
     ON_PLAYING
 }
