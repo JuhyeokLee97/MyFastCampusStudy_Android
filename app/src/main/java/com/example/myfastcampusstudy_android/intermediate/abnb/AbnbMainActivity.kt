af98587154461b485c0da95ba8b507f96961d158
JuhyeokLee97 <zero5.two4@gmail.com>
2021-12-05 21:06:23 +0900

    에어비엔비 - 01. 네이버 지도 API 사용하기

--- app/src/main/java/com/example/myfastcampusstudy_android/intermediate/abnb/AbnbMainActivity.kt
+++ app/src/main/java/com/example/myfastcampusstudy_android/intermediate/abnb/AbnbMainActivity.kt
@@ -3,10 +3,56 @@ package com.example.myfastcampusstudy_android.intermediate.abnb
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myfastcampusstudy_android.R
+import com.naver.maps.map.MapView
 
 class AbnbMainActivity : AppCompatActivity() {
+    private val mapView: MapView by lazy {
+        findViewById(R.id.mapView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_abnb_main)
+
+        mapView.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    private fun initViews() {
+
     }
 }
