6fd0a7a528f09defc9394e942dd0a511eaa028bc
JuhyeokLee97 <zero5.two4@gmail.com>
2021-12-12 22:19:38 +0900

    도서 리뷰 앱 - 인터파크 Open API를 통해 도서 목록 가져오기 (issue: 인터파크에서 API 제공 중단한 듯)

--- app/src/main/java/com/example/myfastcampusstudy_android/intermediate/book_review/BookReviewMainActivity.kt
+++ app/src/main/java/com/example/myfastcampusstudy_android/intermediate/book_review/BookReviewMainActivity.kt
@@ -2,11 +2,56 @@ package com.example.myfastcampusstudy_android.intermediate.book_review
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.myfastcampusstudy_android.R
+import com.example.myfastcampusstudy_android.api.BookService
+import com.example.myfastcampusstudy_android.model.BestSellerDto
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class BookReviewMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_review_main)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://book.interpark.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val bookService = retrofit.create(BookService::class.java)
+
+        bookService.getBestSellerBooks(getString(R.string.interpark_api_key))
+            .enqueue(object : Callback<BestSellerDto> {
+                override fun onResponse(
+                    call: Call<BestSellerDto>,
+                    response: Response<BestSellerDto>
+                ) {
+                    if (response.isSuccessful.not()) {
+                        Log.e(TAG, response.toString())
+                        return
+
+                    }
+
+                    response.body()?.let {
+                        it.books.forEach { book ->
+                            Log.d(TAG, book.toString())
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<BestSellerDto>, t: Throwable) {
+                    // todo 실패처리
+                    Log.e(TAG, t.toString())
+                }
+            })
     }
+
+    companion object {
+        private const val TAG = "BookReviewMainActivity"
+    }
+
 }
