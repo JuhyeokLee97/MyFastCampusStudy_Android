4bb7d272bec38f336ec7461de7ca48cfcb2603a7
JuhyeokLee97 <zero5.two4@gmail.com>
2021-12-12 21:26:14 +0900

    Intermediate 단계 프로젝트 셋팅

---
+++ app/src/main/java/com/example/myfastcampusstudy_android/intermediate/book_review/BookReviewMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myfastcampusstudy_android.intermediate.book_review
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myfastcampusstudy_android.R
+
+class BookReviewMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_review_main)
+    }
+}
