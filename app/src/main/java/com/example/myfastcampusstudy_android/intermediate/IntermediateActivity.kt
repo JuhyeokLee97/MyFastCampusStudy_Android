cddca73d3b8c119346620335200a00a2740d8eef
JuhyeokLee97 <zero5.two4@gmail.com>
2021-12-14 08:03:45 +0900

    중고거래 앱 - 00. 프로젝트 셋팅

--- app/src/main/java/com/example/myfastcampusstudy_android/intermediate/IntermediateActivity.kt
+++ app/src/main/java/com/example/myfastcampusstudy_android/intermediate/IntermediateActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.widget.AppCompatButton
 import com.example.myfastcampusstudy_android.R
 import com.example.myfastcampusstudy_android.intermediate.abnb.AbnbMainActivity
 import com.example.myfastcampusstudy_android.intermediate.book_review.BookReviewMainActivity
+import com.example.myfastcampusstudy_android.intermediate.used_transaction.UsedTransactionMainActivity
 
 class IntermediateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +20,7 @@ class IntermediateActivity : AppCompatActivity() {
     private fun setListener() {
         setListenerToButtonForBookReview()
         setListenerToButtonForAirBNB()
+        setListenerToButtonForUsedTransaction()
     }
 
     private fun setListenerToButtonForBookReview() {
@@ -34,6 +36,15 @@ class IntermediateActivity : AppCompatActivity() {
         val btnMoveToAirBNBMainActivity = findViewById<AppCompatButton>(R.id.btnMoveToAirBNB)
         btnMoveToAirBNBMainActivity.setOnClickListener {
             val intent = Intent(this, AbnbMainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun setListenerToButtonForUsedTransaction() {
+        val btnMoveToUsedTransactionMainActivity =
+            findViewById<AppCompatButton>(R.id.btnMoveToUsedTransaction)
+        btnMoveToUsedTransactionMainActivity.setOnClickListener {
+            val intent = Intent(this, UsedTransactionMainActivity::class.java)
             startActivity(intent)
         }
     }
---
+++ app/src/main/java/com/example/myfastcampusstudy_android/intermediate/used_transaction/UsedTransactionMainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myfastcampusstudy_android.intermediate.used_transaction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myfastcampusstudy_android.R
+
+class UsedTransactionMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_used_transaction_main)
+    }
+}
