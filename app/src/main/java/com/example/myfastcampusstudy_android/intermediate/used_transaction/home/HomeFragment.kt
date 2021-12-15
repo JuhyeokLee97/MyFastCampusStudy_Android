2c4dab4343557f728d5b6ee628f8cc86fb32386f
JuhyeokLee97 <zero5.two4@gmail.com>
2021-12-15 22:02:37 +0900

    중고거래 앱 - 02. 상품 목록 페이지 UI 구성하기(1)  - ViewBinding 셋팅

--- app/src/main/java/com/example/myfastcampusstudy_android/intermediate/used_transaction/home/HomeFragment.kt
+++ app/src/main/java/com/example/myfastcampusstudy_android/intermediate/used_transaction/home/HomeFragment.kt
@@ -1,7 +1,20 @@
 package com.example.myfastcampusstudy_android.intermediate.used_transaction.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.myfastcampusstudy_android.R
+import com.example.myfastcampusstudy_android.databinding.FragmentHomeBinding
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
+
+    private var binding: FragmentHomeBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // onViewCreated 에서 null 체크를 하지 않기 위해서 지역변수 fragmentHomeBinding 사용
+        val fragmentHomeBinding = FragmentHomeBinding.bind(view)
+        binding = fragmentHomeBinding
+    }
 }
