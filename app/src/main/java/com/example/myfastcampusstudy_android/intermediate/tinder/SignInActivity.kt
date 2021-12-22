6ff437b791b690c238f8c98de3d081bdaf31a89c
JuhyeokLee97 <zero5.two4@gmail.com>
2021-12-23 08:37:32 +0900

    틴더 앱 - 01. Firebase Email 로그인 구현하기

---
+++ app/src/main/java/com/example/myfastcampusstudy_android/intermediate/tinder/SignInActivity.kt
@@ -0,0 +1,84 @@
+package com.example.myfastcampusstudy_android.intermediate.tinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import com.example.myfastcampusstudy_android.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class SignInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        initViews()
+    }
+
+    private fun initViews() {
+        initSignInButton()
+        initSignUpButton()
+    }
+
+
+    private fun initSignInButton() {
+        binding.btnSignIn.setOnClickListener {
+            val email = getInputEmail()
+            val password = getInputPassword()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "이메일 또는 비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        finish()
+                    } else {
+                        Toast.makeText(
+                            this,
+                            "로그인에 실패했습니다. 이메일 또는 비밀번호를 확인해주세요.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+    }
+
+    private fun initSignUpButton() {
+        binding.btnSignUp.setOnClickListener {
+            val email = getInputEmail()
+            val password = getInputPassword()
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "이메일 또는 비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            this,
+                            "회원가입에 성공했습니다. 로그인 버튼을 눌러 로그인해주세요.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(this, "이미 가입한 이메일이거나, 회원가입에 실패했습니다.", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+        }
+    }
+
+
+    private fun getInputEmail(): String = binding.etEmail.text.toString()
+    private fun getInputPassword(): String = binding.etPassword.text.toString()
+}
