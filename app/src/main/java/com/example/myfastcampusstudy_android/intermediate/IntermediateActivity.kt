f3d6fae7592ecd264c5137c04993eabcb69130b4
JuhyeokLee97 <zero5.two4@gmail.com>
2021-12-23 07:48:14 +0900

    Refactoring for view-binding in Intermediate Package

--- app/src/main/java/com/example/myfastcampusstudy_android/intermediate/IntermediateActivity.kt
+++ app/src/main/java/com/example/myfastcampusstudy_android/intermediate/IntermediateActivity.kt
@@ -1,20 +1,34 @@
 package com.example.myfastcampusstudy_android.intermediate
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import androidx.appcompat.widget.AppCompatButton
 import com.example.myfastcampusstudy_android.R
+import com.example.myfastcampusstudy_android.databinding.ActivityIntermediateBinding
 import com.example.myfastcampusstudy_android.intermediate.abnb.AbnbMainActivity
 import com.example.myfastcampusstudy_android.intermediate.book_review.BookReviewMainActivity
 import com.example.myfastcampusstudy_android.intermediate.used_transaction.UsedTransactionMainActivity
 
 class IntermediateActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntermediateBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intermediate)
-
         setListener()
+    }
+
+    override fun onCreateView(
+        parent: View?,
+        name: String,
+        context: Context,
+        attrs: AttributeSet
+    ): View? {
+        binding = ActivityIntermediateBinding.bind(parent!!)
+        return super.onCreateView(parent, name, context, attrs)
     }
 
     private fun setListener() {
@@ -24,26 +38,21 @@ class IntermediateActivity : AppCompatActivity() {
     }
 
     private fun setListenerToButtonForBookReview() {
-        val btnMoveToBookReviewMainActivity =
-            findViewById<AppCompatButton>(R.id.btnMoveToBookReview)
-        btnMoveToBookReviewMainActivity.setOnClickListener {
+        binding.btnMoveToBookReview.setOnClickListener {
             val intent = Intent(this, BookReviewMainActivity::class.java)
             startActivity(intent)
         }
     }
 
     private fun setListenerToButtonForAirBNB() {
-        val btnMoveToAirBNBMainActivity = findViewById<AppCompatButton>(R.id.btnMoveToAirBNB)
-        btnMoveToAirBNBMainActivity.setOnClickListener {
+        binding.btnMoveToAirBNB.setOnClickListener {
             val intent = Intent(this, AbnbMainActivity::class.java)
             startActivity(intent)
         }
     }
 
     private fun setListenerToButtonForUsedTransaction() {
-        val btnMoveToUsedTransactionMainActivity =
-            findViewById<AppCompatButton>(R.id.btnMoveToUsedTransaction)
-        btnMoveToUsedTransactionMainActivity.setOnClickListener {
+        binding.btnMoveToUsedTransaction.setOnClickListener {
             val intent = Intent(this, UsedTransactionMainActivity::class.java)
             startActivity(intent)
         }
