8389dce7b3412ce6785a007e139cc983fa957592
JuhyeokLee97 <zero5.two4@gmail.com>
2021-12-29 23:47:23 +0900

    BMI 계산기 프로젝트 리팩토링  - 리소스 처리  - 뷰바인딩

--- app/src/main/java/com/example/myfastcampusstudy_android/basic/bmi_calculator/ResultActivity.kt
+++ app/src/main/java/com/example/myfastcampusstudy_android/basic/bmi_calculator/ResultActivity.kt
@@ -2,36 +2,47 @@ package com.example.myfastcampusstudy_android.basic.bmi_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
 import com.example.myfastcampusstudy_android.R
+import com.example.myfastcampusstudy_android.databinding.ActivityResultBinding
 import kotlin.math.pow
 
 class ResultActivity : AppCompatActivity() {
-    val TAG = "ResultActivity"
+    private lateinit var binding: ActivityResultBinding
+    private var bmi: Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        initViews()
+    }
+
+    private fun initViews() {
+        initBmiValueTextView()
+        initResultStatusTextView(bmi)
+    }
+
+    private fun initBmiValueTextView() {
         val height = intent.getIntExtra("height", 0)
         val weight = intent.getIntExtra("weight", 0)
 
-        Log.d(TAG, "height: $height \t weight: $weight")
+        bmi = weight / (height / 100.0).pow(2.0)
 
-        val bmi = weight / (height / 100.0).pow(2.0)
-        val resultStatus = when{
-            bmi >= 35.0 -> "고도 비만"
-            bmi >= 30.0 -> "중정도 비만"
-            bmi >= 25.0 -> "경도 비만"
-            bmi >= 23.0 -> "과체중"
-            bmi >= 18.5 -> "정상체중"
-            else -> "저체중"
+        binding.tvBmiValue.text = bmi.toString()
+    }
+
+    private fun initResultStatusTextView(bmi: Double) {
+        val resultStatus = when {
+            bmi >= 35.0 -> getString(R.string.text_bmi_level_5)
+            bmi >= 30.0 -> getString(R.string.text_bmi_level_4)
+            bmi >= 25.0 -> getString(R.string.text_bmi_level_3)
+            bmi >= 23.0 -> getString(R.string.text_bmi_level_2)
+            bmi >= 18.5 -> getString(R.string.text_bmi_level_1)
+            else -> getString(R.string.text_bmi_level_0)
         }
 
-        val tvBmiValue: TextView = findViewById(R.id.tvBmiValue)
-        val tvResultStatus: TextView = findViewById(R.id.tvResultStatus)
-
-        tvBmiValue.text = bmi.toString()
-        tvResultStatus.text = resultStatus
+        binding.tvResultStatus.text = resultStatus
     }
+
 }
