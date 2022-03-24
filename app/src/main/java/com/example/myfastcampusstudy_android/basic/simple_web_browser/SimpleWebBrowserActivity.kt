17f055d7ec802d6210334a3534cbf6901cc59b83
JuhyeokLee97 <zero5.two4@gmail.com>
2022-03-24 21:56:39 +0900

    #48 심플 웹 브라우저(2)  - URL 로딩 기능 구현하기

--- app/src/main/java/com/example/myfastcampusstudy_android/basic/simple_web_browser/SimpleWebBrowserActivity.kt
+++ app/src/main/java/com/example/myfastcampusstudy_android/basic/simple_web_browser/SimpleWebBrowserActivity.kt
@@ -2,6 +2,8 @@ package com.example.myfastcampusstudy_android.basic.simple_web_browser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebViewClient
 import com.example.myfastcampusstudy_android.databinding.ActivitySimpleWebBrowserBinding
 
 class SimpleWebBrowserActivity : AppCompatActivity() {
@@ -12,10 +14,31 @@ class SimpleWebBrowserActivity : AppCompatActivity() {
         binding = ActivitySimpleWebBrowserBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initView()
+        initViews()
+        bindViews()
     }
 
-    private fun initView() {
+    private fun initViews() {
+        binding.apply {
+            webView.apply {
+                webViewClient =
+                    WebViewClient()         // 우리가 생성한 웹뷰를 사용하기 위함 == 외부 웹 어플리케이션을 이용하지 않을 수 있음.
+                settings.javaScriptEnabled = true       // JavaScript 사용을 허용한다. For 웹 이벤트를 위해
+                webView.loadUrl("http://www.google.com")
+            }
+        }
+    }
 
+
+    private fun bindViews() {
+        binding.apply {
+            etSearch.setOnEditorActionListener { view, actionId, _ ->
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    webView.loadUrl(view.text.toString())
+                }
+
+                return@setOnEditorActionListener false
+            }
+        }
     }
 }
