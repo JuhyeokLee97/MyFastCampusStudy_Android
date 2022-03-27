a953567d938372f30fe8a916b3ba8407ec57c4d7
JuhyeokLee97 <zero5.two4@gmail.com>
2022-03-27 18:31:01 +0900

    #48 심플 웹 브라우저: 완성도 높이기(2)  - progressBar 추가

--- app/src/main/java/com/example/myfastcampusstudy_android/basic/simple_web_browser/SimpleWebBrowserActivity.kt
+++ app/src/main/java/com/example/myfastcampusstudy_android/basic/simple_web_browser/SimpleWebBrowserActivity.kt
@@ -1,8 +1,10 @@
 package com.example.myfastcampusstudy_android.basic.simple_web_browser
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.example.myfastcampusstudy_android.databinding.ActivitySimpleWebBrowserBinding
@@ -31,7 +33,9 @@ class SimpleWebBrowserActivity : AppCompatActivity() {
         binding.apply {
             webView.apply {
                 webViewClient =
-                    WebViewClient()         // 우리가 생성한 웹뷰를 사용하기 위함 == 외부 웹 어플리케이션을 이용하지 않을 수 있음.
+                    WebViewClient()                     // 우리가 생성한 웹뷰를 사용하기 위함 == 외부 웹 어플리케이션을 이용하지 않을 수 있음.
+                webChromeClient =
+                    WebChromeClient()     // ProgressBar 사용을 위해, WebView에서 다양한 기능을 사용하기 위해서는 WebChromClient도 사용해주면 된다.
                 settings.javaScriptEnabled = true       // JavaScript 사용을 허용한다. For 웹 이벤트를 위해
                 webView.loadUrl(DEFAULT_URL)
             }
@@ -57,13 +61,33 @@ class SimpleWebBrowserActivity : AppCompatActivity() {
         }
     }
 
-    inner class WebViewClient: android.webkit.WebViewClient(){
+    inner class WebViewClient : android.webkit.WebViewClient() {
+
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+
+            binding.progressBar.show()
+        }
+
         override fun onPageFinished(view: WebView?, url: String?) {
             super.onPageFinished(view, url)
 
-            binding.refreshLayout.isRefreshing = false
+            binding.apply {
+                refreshLayout.isRefreshing = false
+                progressBar.hide()
+            }
+
         }
     }
+
+    inner class WebChromeClient : android.webkit.WebChromeClient() {
+        override fun onProgressChanged(view: WebView?, newProgress: Int) {
+            super.onProgressChanged(view, newProgress)
+
+            binding.progressBar.progress = newProgress
+        }
+    }
+
     companion object {
         private const val DEFAULT_URL = "http://www.google.com"
     }
